fa4a9765cec3b68015bcdd9e6c7c971a2cb23f7b
dfreeman3102 <davidfreeman3102@gmail.com>
2024-02-17 22:08:55 -0500

    added function to allow button to ask requirements for password generation

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+
+var length = prompt("How many characters between 8-128?", "8");
+console.log(length);
+
+var letters = confirm ("Include letters?");
+console.log(letters);
+
+var numbers = confirm("Include numbers?");
+console.log(numbers);
+
+var specialChar = confirm("Include special characters?");
+console.log(specialChar);
+
+var upper = confirm("Include uppercase?");
+console.log(upper);
+
+var lower = confirm("Include lowercase?");
+console.log(lower);
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
