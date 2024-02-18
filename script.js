1e50844b273a2db17f516cc34fc2b32b6d00c144
dfreeman3102 <davidfreeman3102@gmail.com>
2024-02-18 14:17:34 -0500

    formatted the doc to make it look cleaner

--- script.js
+++ script.js
@@ -4,47 +4,47 @@ var generateBtn = document.querySelector("#generate");
 // this function generates the password based on parameters
 function generatePassword(length, numbers, specialChar, upper, lower) {
 
-// these prompts and variables give the funcion the parameter answers
-var length = prompt("How many characters between 8-128?", "8");
-console.log(length);
+  // these prompts and variables give the funcion the parameter answers
+  var length = prompt("How many characters between 8-128?", "8");
+  console.log(length);
 
-var numbers = confirm("Include numbers?");
-console.log(numbers);
+  var numbers = confirm("Include numbers?");
+  console.log(numbers);
 
-var specialChar = confirm("Include special characters?");
-console.log(specialChar);
+  var specialChar = confirm("Include special characters?");
+  console.log(specialChar);
 
-var upper = confirm("Include uppercase?");
-console.log(upper);
+  var upper = confirm("Include uppercase?");
+  console.log(upper);
 
-var lower = confirm("Include lowercase?");
-console.log(lower);
+  var lower = confirm("Include lowercase?");
+  console.log(lower);
 
-var passwordLength = length;
+  var passwordLength = length;
 
-var includeNumbers = numbers;
+  var includeNumbers = numbers;
 
-var includeSpecial = specialChar;
+  var includeSpecial = specialChar;
 
-var includeUpper = upper;
+  var includeUpper = upper;
 
-var includeLower = lower;
+  var includeLower = lower;
 
   // these variables give all the possible options for characters
   var lowerChars = "abcdefghijklmnopqrstuvwxyz";
   var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numberChars = "0123456789";
   var allowedSpecial = "`~!@#$%^&*()-_=+[{]};<>/?";
-// this var gives the allowed characters a place to go for picking
+  // this var gives the allowed characters a place to go for picking
   var allowedChars = "";
 
   var password = "";
-// these statements say if they are included, go into allowedChars, if not do nothing
+  // these statements say if they are included, go into allowedChars, if not do nothing
   allowedChars += includeLower ? lowerChars : "";
   allowedChars += includeUpper ? upperChars : "";
   allowedChars += includeNumbers ? numberChars : "";
   allowedChars += includeSpecial ? allowedSpecial : "";
-// if statements for the inputs of length and what characters are allowed
+  // if statements for the inputs of length and what characters are allowed
   if (passwordLength < 8 || passwordLength > 128) {
     return "*-Password must be between 8 and 128 Characters-*";
   }
